aa933d6631a9003c92eacdb1234f0d7ab5f0987c
Naomi Thomson <naomithomson94@gmail.com>
2017-09-25 15:27:54 -0700

    add modal to selected option

---
+++ src/components/Example.js
@@ -0,0 +1,14 @@
+const Example = (props) => {
+  console.log(props)
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+}
+
+export default Example
+
+
+    
+
---
+++ src/temp/app.js
@@ -0,0 +1,3 @@
+// stateless functional component
+// no state, uses function (not class), no "this", presentational
+// faster than class based component
